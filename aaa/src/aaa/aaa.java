df51ab3daf8ea0e31a024337c657c88f3133a21a
Razor5705 <nikkmed805@gmail.com>
2024-01-09 10:04:56 +0100

    HOLA PRIMER COMMIT

---
+++ aaa/src/aaa/aaa.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package aaa;
+
+/**
+ * 
+ */
+public class aaa {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
