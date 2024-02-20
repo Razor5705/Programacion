eb150268db3b0f1a7dcc3948360a6a3a2a4e95ca
Nikolas Medina <nikkmed805@gmail.com>
2024-02-20 10:51:24 +0100

    EJER 01 DE LA PRACTICA

--- POO/src/PRACTICA/SistemadeNotificaciones.java
+++ POO/src/PRACTICA/SistemadeNotificaciones.java
@@ -4,8 +4,16 @@ public class SistemadeNotificaciones {
 
 	public static void main(String[] args) {
 	CorreoElectronico Correo = new CorreoElectronico("Lucas", "Deseo enviarte este mensaje, este mensajeeee", "Tarea");
+	SMS numero = new SMS("Messi","Ojala vuelvas al barça", 200);
+    NotificarApp aplicacion = new NotificarApp("Nikolas", "Nueva Temporada de Brawl Stars", null);
 	
-
+    Correo.enviar();
+    System.out.println();
+    numero.enviar();
+    System.out.println();
+    aplicacion.enviar();
+    System.out.println();
+    
 	}
 
 }
@@ -18,15 +26,21 @@ abstract class Notificacion {
 
 class CorreoElectronico extends Notificacion{
 
-	public CorreoElectronico (String destinatario, String mensaje, String Asunto) {
+	String asunto;
+	
+	public CorreoElectronico (String destinatario, String mensaje, String asunto) {
 		this.destinatario = destinatario;
 		this.mensaje = mensaje;
+		this.asunto = asunto;
 	}
 		
 
 	@Override
 	void enviar() {
 		// TODO Auto-generated method stub
+		System.out.println("Destinatario: " + this.destinatario);
+		System.out.println("Mensaje: " + this.mensaje);
+       System.out.println("Asunto: " + this.asunto);
 		
 	}
 	
@@ -34,30 +48,39 @@ class CorreoElectronico extends Notificacion{
 
 class SMS extends Notificacion{
 
-	public SMS (String destinatario, String mensaje, String limiteCaracteres) {
+	int limiteCaracteres;
+	
+	public SMS (String destinatario, String mensaje, int limiteCaracteres) {
 		this.destinatario = destinatario;
 		this.mensaje = mensaje;
-
+  this.limiteCaracteres = limiteCaracteres;
 	}
 	
 	@Override
 	void enviar() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Destinatario: " + this.destinatario);
+		System.out.println("Mensaje: " + this.mensaje);
+		System.out.println("El limite de Caracteres es: " + this.limiteCaracteres );
 	}
 	
 }
 
 class NotificarApp extends Notificacion{
 
-	public NotificarApp (String destinatario, String mensaje, String Idenfiticador) {
+	Boolean Identificador;
+	
+	public NotificarApp (String destinatario, String mensaje,  Boolean Identificador) {
 		this.destinatario = destinatario;
 		this.mensaje = mensaje;
+		this.Identificador = true;
 	}
 	
 	@Override
 	void enviar() {
-
+		System.out.println("Destinatario: " + this.destinatario);
+		System.out.println("Mensaje: " + this.mensaje);
+		System.out.println("Identificador si es True o False: " + this.Identificador);
 	}
 	
 }
