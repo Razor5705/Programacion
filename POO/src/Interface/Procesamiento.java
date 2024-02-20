320a95f53e19234bb33659bc3db064ff70924d06
Nikolas Medina <nikkmed805@gmail.com>
2024-02-20 10:05:01 +0100

    Ejer 02 - No entiendo el ejercicio

---
+++ POO/src/Interface/Procesamiento.java
@@ -0,0 +1,75 @@
+package Interface;
+
+public class Procesamiento {
+
+	public static void main(String[] args) {
+
+		ProcesadordePago proveedorA = new Proveedor1();
+		ProcesadordePago proveedorB = new Proveedor2();
+		
+		Cliente clienteA = new Cliente(proveedorA);
+		Cliente clienteB = new Cliente(proveedorA);
+
+		clienteA.Reembolsarpago();
+		clienteB.Realizarpago();
+	}
+
+}
+
+interface ProcesadordePago {
+	void Realizarpago();
+	void Reembolsarpago();
+}
+
+class Proveedor1 implements ProcesadordePago{
+
+	@Override
+	public void Realizarpago() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Reembolsarpago() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+class Proveedor2 implements ProcesadordePago {
+
+	@Override
+	public void Realizarpago() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Reembolsarpago() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+class Cliente {
+	
+	private ProcesadordePago procesadordePago;
+	
+	public Cliente (ProcesadordePago procesadordePago) {
+		this.procesadordePago = procesadordePago;
+	}
+	
+	public void Realizarpago() {
+
+    procesadordePago.Realizarpago();		
+	}
+
+	public void Reembolsarpago() {
+		// TODO Auto-generated method stub
+	    procesadordePago.Reembolsarpago();			
+	}
+	
+	
+}
