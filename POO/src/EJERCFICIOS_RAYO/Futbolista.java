788d8d42cef4d54e6d6a1c6b082cb52a90faa33a
Nikolas Medina <nikkmed805@gmail.com>
2024-02-28 12:46:11 +0100

    Interfaces Final - Rayo

--- POO/src/EJERCFICIOS_RAYO/Futbolista.java
+++ POO/src/EJERCFICIOS_RAYO/Futbolista.java
@@ -23,6 +23,33 @@ public Futbolista(int dorsal, String demarcacion, int id, String nombre, String 
 		this.demarcacion = demarcacion;
 	}
  
+
+@Override
+public void concentrarse() {
+	// TODO Auto-generated method stub
+System.out.println("Concentrarse (Clase Futbolista). ");
+}
+
+@Override
+public void viajar() {
+	// TODO Auto-generated method stub
+	   System.out.println("Viajar (Clase Futbolista). ");
+
+}
+
+@Override
+public void entrenar() {
+	// TODO Auto-generated method stub
+	   System.out.println("Entrenarse (Clase Futbolista). ");
+
+}
+
+@Override
+public void jugarPartido() {
+	// TODO Auto-generated method stub
+	   System.out.println("Asistir al partido (Clase Futbolista). ");
+
+}
 public int getDorsal() {
 	return dorsal;
 }
---
+++ POO/src/EJERCFICIOS_RAYO/Inicial.java
@@ -0,0 +1,17 @@
+
+package EJERCFICIOS_RAYO;
+
+public class Inicial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Futbolista rayo = new Futbolista(6, "Mediocampista", 10, "Jose Manuel", "Rayo", 22);
+       
+       rayo.concentrarse();
+       rayo.entrenar();
+       rayo.jugarPartido();
+       rayo.viajar();
+       
+	}
+
+}
